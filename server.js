3a8d6725837f477c3e9bbdbaf522476e2da47421
Dev-Kevin012 <142422385+Dev-Kevin012@users.noreply.github.com>
2023-09-02 19:56:39 +0530

    Code CleanUp

--- server.js
+++ server.js
@@ -1,46 +1,6 @@
 const http = require("http");
-const fs = require("fs");
+const routes = require("./routes");
 
-const server = http.createServer((req, res) => {
-  const url = req.url;
-  if (url === "/") {
-    fs.readFile("message.txt", (err, data) => {
-      if (err) {
-        console.error(err);
-        res.statusCode = 500;
-        res.end("Internal Server Error");
-        return;
-      }
-      let message = data.toString();
-
-      res.write("<html>");
-      res.write("<head><title>Enter Message</title></head>");
-      res.write("<body>");
-      res.write(`<p>${message}</p>`);
-      res.write("<form method='POST' action='/submit'>");
-      res.write("<input type='text' name='message'>");
-      res.write("<button type='submit'>Submit</button>");
-      res.write("</form>");
-      res.write("</body>");
-      res.write("</html>");
-      res.end();
-    });
-  } else if (url === "/submit" && req.method === "POST") {
-    const body = [];
-    req.on("data", (chunk) => {
-      body.push(chunk);
-    });
-    req.on("end", () => {
-      const parsedbody = Buffer.concat(body).toString();
-      const message = parsedbody.split("=")[1];
-      fs.writeFile("message.txt", message, (err) => {
-        if (err) console.log(err);
-        res.statusCode = 302;
-        res.setHeader("Location", "/");
-        res.end();
-      });
-    });
-  }
-});
+const server = http.createServer(routes);
 
 server.listen(3000, () => console.log("Server is Running!"));
